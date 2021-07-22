409d324294b670bbb71f31d3650d8b6f0cc6c14f
Dylan R Hewitt <65405122+DReece-dev@users.noreply.github.com>
2021-07-22 13:22:38 +0100

    Added flyout menu

--- scripts/slushii.js
+++ scripts/slushii.js
@@ -19,3 +19,21 @@ window.onclick = function (event) {
         }
     }
 }
+
+// Flyout menu 1
+function myFunction2() {
+    document.getElementById("flyout-dropdown").classList.toggle("flyout-dropdown-show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.flyout-dropbutton')) {
+        let dropdowns = document.getElementsByClassName("flyout-dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('flyout-dropdown-show')) {
+                openDropdown.classList.remove('flyout-dropdown-show');
+            }
+        }
+    }
+}
