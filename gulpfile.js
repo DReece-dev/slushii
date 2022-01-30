80c48c44a57592b85c132cb6289f5223598bf2e5
Dylan Reece <mrdylanreece@gmail.com>
2022-01-30 12:11:13 +0000

    Big Update
    Now using gulp, gulp-sass and postcss

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+// const postcssPresetEnv = require('postcss-preset-env');
+
+gulp.task('css', function () {
+    let processors = [
+        autoprefixer
+    ];
+    return gulp.src('./src/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('purgecss', () => {
+    return gulp.src('./src/*.scss')
+        .pipe(purgecss({
+            content: ['src/**/*.html']
+        }))
+        .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('mini', () => {
+    let nano = [
+        cssnano
+    ];
+    return gulp.src('./dist/*.css')
+        .pipe(postcss(nano))
+        .pipe(gulp.dest('./dist'));
+});
