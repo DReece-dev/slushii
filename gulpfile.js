ab633ea2801e3e738ab257fa55f2bf1cf57cba9c
Dylan Reece <mrdylanreece@gmail.com>
2023-01-28 23:26:38 +0000

    Gulp files improved

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,18 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
 
 gulp.task('css', function () {
     let processors = [
         autoprefixer
     ];
     return gulp.src('./src/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(processors))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist'));
 });
 
@@ -22,14 +26,17 @@ gulp.task('purgecss', () => {
             content: ['src/**/*.html'],
             defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
         }))
+        .pipe(concat('all.css'))
         .pipe(gulp.dest('./dist'))
-})
+});
 
 gulp.task('mini', () => {
     let nano = [
         cssnano
     ];
-    return gulp.src('./dist/**/*.css')
+    return gulp.src('./dist/all.css')
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(postcss(nano))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist'));
 });
