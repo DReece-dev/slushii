94db5212a7e79a0bb871ad6229a4e73a66422e58
Dylan Reece <mrdylanreece@gmail.com>
2023-01-29 00:17:15 +0000

    Updated Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,38 +5,44 @@ const purgecss = require('gulp-purgecss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
+const gulpif = require('gulp-if');
+const rename = require('gulp-rename');
+const mergeMediaQueries = require('gulp-merge-media-queries');
+const plumber = require('gulp-plumber');
+const cached = require('gulp-cached');
 
 gulp.task('css', function () {
     let processors = [
         autoprefixer
     ];
     return gulp.src('./src/*.scss')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
+        .pipe(cached('css'))
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('purgecss', () => {
-    return gulp.src('./dist/**/*.css')
         .pipe(purgecss({
             safelist: [],
             content: ['src/**/*.html'],
             defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
         }))
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('./dist'))
+        .pipe(mergeMediaQueries())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('mini', () => {
     let nano = [
         cssnano
     ];
-    return gulp.src('./dist/all.css')
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(postcss(nano))
-        .pipe(sourcemaps.write('.'))
+    return gulp.src('./dist/*.css')
+        .pipe(gulpif(process.env.NODE_ENV === 'production', postcss(nano)))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('./src/*.scss', gulp.series('css'));
+});
+
+gulp.task('default', gulp.series(gulp.parallel('css', 'mini'), 'watch'));
